a1e66954a730e15cdc0a371e053bd0547e1e91ab
Siddhu-2727 <146078193+Siddhu-2727@users.noreply.github.com>
2024-05-28 21:17:29 +0530

    MySQL Local.session.sql

--- MySQL Local.session.sql
+++ MySQL Local.session.sql
@@ -1,4 +1,17 @@
+CREATE TABLE companies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    type ENUM('admin', 'user', 'company') NOT NULL
+);
 
 INSERT INTO users (username, password, type) VALUES
 ('admin1', 'password1', 'admin'),
